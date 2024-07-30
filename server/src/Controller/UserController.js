2d975909f82a29fa645a331cd4e4092a0ac1226e
nanthapong1234 <takbig47@gmail.com>
2024-07-30 09:58:37 +0700

    folder perpared

--- server/src/Controller/UserController.js
+++ server/src/Controller/UserController.js
@@ -1,26 +1,76 @@
+const { User } = require('../models')
 
-module.exports = { 
-    // get all user 
-    index (req, res) { 
-    res.send('ดูข้อมูล user ทุกคน') 
-    }, 
-
-    // create user 
-    create (req, res) { 
-    res.send('ทําการสร้างผู ้ใช้งาน: ' + JSON.stringify(req.body.username));
-    }, 
-
-    // put user
-    put(req, res){
-        res.send('แก้ไข user คนที่ ' + req.params.userId + 'ข้อมูลที่แก้ไข ' + JSON.stringify(req.body));
-    },
-
-    //remove user
-    remove(req, res){
-        res.send('ลบ user คนที่ ' +req.params.userId);
-    },
-      // get user by id 
-      show (req, res) { 
-        res.send('ดูข้อมูลผู ้ใช้งานทั้งหมด')
-      }
+module.exports = {
+  // get all user 
+  async index(req, res) {
+    try {
+      const users = await User.findAll()
+      res.send(users)
+    } catch (err) {
+      res.status(500).send({
+        error: 'The users information was incorrect'
+      })
     }
+  },
+  // create user 
+  async create(req, res) {
+    try {
+      const user = await User.create(req.body)
+      res.send(user.toJSON())
+    } catch (err) {
+      res.status(500).send({
+        error: 'Create user incorrect'
+      })
+    }
+  },
+  // put user
+  async put(req, res) {
+    try {
+      await User.update(req.body, {
+        where: {
+          id: req.params.userId
+        }
+      })
+      res.send(req.body)
+    } catch (err) {
+      res.status(500).send({
+        error: 'Update user incorrect'
+      })
+    }
+  },
+  //remove user
+  async remove(req, res) {
+    try {
+      const user = await User.findOne({
+        where: {
+          id: req.params.userId
+        }
+      })
+
+      if (!user) {
+        return res.status(403).send({
+          error: 'The user information was incorrect'
+        })
+      }
+
+      await user.destroy()
+      res.send(user)
+    } catch (err) {
+      res.status(500).send({
+        error: 'The user information was incorrect'
+      })
+    }
+  },
+
+  // get user by id 
+  async show(req, res) {
+    try {
+      const user = await User.findByPk(req.params.userId)
+      res.send(user)
+    } catch (err) {
+      res.status(500).send({
+        error: 'The user information was incorrect'
+      })
+    }
+  },
+}  
--- server/src/models/index.js
+++ server/src/models/index.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const path = require('path')
 const Sequelize = require('sequelize')
 const config = require('../config/config');
-const db = {} 
+const db = {}
 
 const sequelize = new Sequelize(
     config.db.database,
